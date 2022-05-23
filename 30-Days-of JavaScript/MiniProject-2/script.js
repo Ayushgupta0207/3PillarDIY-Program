ac661671991ffe5ad29d641c350d2a675e96199d
Ayushgupta0207 <ayushgupta1915@gmail.com>
2022-05-23 18:28:39 +0530

    new added

--- 30-Days-of JavaScript/MiniProject-2/script.js
+++ 30-Days-of JavaScript/MiniProject-2/script.js
@@ -1,29 +1,93 @@
+// Selectors
+var todoInput = document.querySelector(".fname");
+var lname=document.querySelector(".lname");
+var country=document.querySelector(".country");
+var score=document.querySelector(".score");
+var btn = document.querySelector(".submit");
+var todolist = document.querySelector(".todo-list");
 
-
-let players = [];
-
-const addPlayer = (e) => {
+//Event Handlers
+btn.onclick = create;
+todolist.onclick = deletecheck;
+var co;
+//Functions
+function create(e) {
+  if (todoInput.value != "") {
     e.preventDefault();
 
-    let player = {
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("todo");
 
-    firstName : document.getElementById('firstName').value,
-    lastName : document.getElementById('lastName').value,
-    country : document.getElementById('country').value,
-    score :   document.getElementById('score').value,
-    }
+    var newLi = document.createElement("li");
+    newLi.classList.add("todo-item1");
+    newLi.innerHTML = todoInput.value;
+    newDiv.appendChild(newLi);
+    
+    var newLi1 = document.createElement("li");
+    newLi1.classList.add("todo-item2");
+    newLi1.innerHTML = lname.value;
+    newDiv.appendChild(newLi1);
 
-    players.push(player);
-    document.forms[0].reset();
+    var newLi2 = document.createElement("li");
+    newLi2.classList.add("todo-item3");
+    newLi2.innerHTML = country.value;
+    newDiv.appendChild(newLi2);
 
-    console.warn('added' , {player});
+    var newLi3 = document.createElement("li");
+    newLi3.classList.add("todo-item4");
+    co=score.value;
+    newLi3.innerHTML = score.value;
+    newDiv.appendChild(newLi3);
 
-    console.log(players);
+    var plusbtn = document.createElement("button");
+    plusbtn.classList.add("plus-btn");
+    plusbtn.innerHTML = '+5';
+    newDiv.appendChild(plusbtn);
 
+    var minusbtn = document.createElement("button");
+    minusbtn.classList.add("minus-btn");
+    minusbtn.innerHTML = '-5';
+    newDiv.appendChild(minusbtn);
+    
+    var deletebtn = document.createElement("button");
+    deletebtn.classList.add("delete-btn");
+    deletebtn.innerHTML = '<i class="fa fa-trash"></i>';
+    newDiv.appendChild(deletebtn);
 
+    todolist.appendChild(newDiv);
+    todoInput.value = "";
+    lname.value="";
+    country.value="";
+    score.value="";
+
+  } else {
+    alert("Input Field Can't Be Blank");
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('btn').addEventListener('click', addPlayer);
-})
+function deletecheck(e) {
+  var item = e.target;
 
+  if (item.classList[0] === "delete-btn") {
+    var parent = item.parentElement;
+    parent.remove();
+  }
+
+  if (item.classList[0] === "plus-btn") {
+    const val=item.parentElement.querySelector(".todo .todo-item4");
+    co=parseInt(co)+5;
+     val.innerHTML=co;
+     
+  }
+
+  if (item.classList[0] === "minus-btn") {
+    const val=item.parentElement.querySelector(".todo .todo-item4");
+    co=parseInt(co)-5;
+    if(parseInt(co)>0){
+     val.innerHTML=co;
+    }
+    else{
+      val.innerHTML=0;
+    }
+  }
+}
